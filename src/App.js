68fefeb5ea326436e2ccbcbb68e45e189a211a7c
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-13 21:10:44 +0100

    Created search movies function and added it into useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
 
 const API_URL = "http://www.omdbapi.com?apikey=3bc9520b";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    // search for all movies based on title
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
   return <h1>App</h1>;
 };
 
