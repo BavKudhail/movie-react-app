835129272b1ccfb5a0c7bbeee8cce03e9c433b69
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-13 22:55:56 +0100

    added further css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const API_URL = "http://www.omdbapi.com?apikey=3bc9520b";
 const App = () => {
   // movies hook
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  //   const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     // search for all movies based on title
@@ -19,26 +19,28 @@ const App = () => {
     setMovies(data.Search);
   };
 
+  searchMovies("batman");
+
   return (
     <div className="app">
       <h1>MOVIEMIX</h1>
       <div className="search">
         <input
           placeholder="search for movies"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          //   value={searchTerm}
+          //   onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={searchIcon}
           alt="search"
-          onClick={() => searchMovies(searchTerm)}
+          //   onClick={() => searchMovies(searchTerm)}
         />
       </div>
       {movies.length > 0 ? (
         movies.map((movie) => {
           return (
             <div className="container">
-              <MovieCard movie1={movie} />
+              <MovieCard className="movie" movie1={movie} />
             </div>
           );
         })
